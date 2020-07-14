6211c4554119c867912569a4e1f45f726300d574
Giorgi Shengelaia <g.shengelaia2001@gmail.com>
2020-07-14 19:52:42 +0400

    build

---
+++ build/precache-manifest.0e8eec990e84b43ba7e021160f8062ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12d03cc20f150a9642324b2df569a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2e3494beca806bfff1",
+    "url": "/static/css/main.79ba9feb.chunk.css"
+  },
+  {
+    "revision": "6bfabe165c0d30957176",
+    "url": "/static/js/2.1458f4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1458f4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2e3494beca806bfff1",
+    "url": "/static/js/main.e83024dc.chunk.js"
+  },
+  {
+    "revision": "674ac8955cd16d5c8501",
+    "url": "/static/js/runtime-main.aad17668.js"
+  }
+]);
