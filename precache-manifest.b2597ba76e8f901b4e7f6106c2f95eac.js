011e6c22d783de123a3b322ddf438efa4f025ace
Giorgi Shengelaia <g.shengelaia2001@gmail.com>
2020-07-14 21:43:36 +0400

    Updates

---
+++ precache-manifest.b2597ba76e8f901b4e7f6106c2f95eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503840409fff22725f309290caa476f4",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "4b2e3494beca806bfff1",
+    "url": "/library-react/static/css/main.79ba9feb.chunk.css"
+  },
+  {
+    "revision": "6bfabe165c0d30957176",
+    "url": "/library-react/static/js/2.1458f4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-react/static/js/2.1458f4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2e3494beca806bfff1",
+    "url": "/library-react/static/js/main.e83024dc.chunk.js"
+  },
+  {
+    "revision": "5eab4539a2b11719e949",
+    "url": "/library-react/static/js/runtime-main.bc126f7f.js"
+  }
+]);
